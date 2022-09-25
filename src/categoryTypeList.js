d30e9b05f309502641997ef63233a3299513c8f9
adi1111 <holubeckiadrian@gmail.com>
2022-09-25 19:56:41 +0200

    work on building basic app structure

---
+++ src/categoryTypeList.js
@@ -0,0 +1,50 @@
+export default function render(){
+
+    const container = document.querySelector('.section-left');
+
+        const list = document.createElement('ul');
+            const inputAndButton = document.createElement('li');
+                const input = document.createElement('input');
+                    input.classList.add('new-category');
+                    input.setAttribute('type', 'text');
+                    input.setAttribute('placeholder', 'Create new category');
+                    input.setAttribute('name', 'cat');
+                const button = document.createElement('button');
+                    button.classList.add('category-button');
+                    button.setAttribute('type', 'button');
+                    button.setAttribute('name', 'cat');
+                    button.textContent = 'Add';
+            inputAndButton.appendChild(input);
+            inputAndButton.appendChild(button); 
+
+            const title = document.createElement('li');
+                title.textContent = 'Category type:';
+        list.appendChild(inputAndButton);
+        list.appendChild(title);
+    
+    const categories = [
+        {
+            text: 'Normal',
+            value: 'normal',
+        },
+        {
+            text: 'Project',
+            value: 'project',
+        },
+        {
+            text: 'Custom',
+            value: 'custom',
+        }
+    ]
+    categories.forEach(item => {
+        const li = document.createElement('li');
+            const radio = document.createElement('input');
+                radio.setAttribute('type', 'radio');
+                radio.setAttribute('name', 'project-type');
+                radio.setAttribute('value', `${item.value}`);
+            li.appendChild(radio);
+            li.textContent = item.text;
+        list.appendChild(li);
+    })
+
+}
---
+++ src/mainPage.js
@@ -0,0 +1,29 @@
+export default function render(){
+    
+    let logoText = 'TO DO';
+
+    const container = document.querySelector('.container');
+    const navbar = document.createElement('div');
+        navbar.classList.add('navbar');
+        const logo = document.createElement('div');
+            logo.classList.add('logo');
+            logo.textContent = logoText;
+        navbar.appendChild(logo);
+    const sectionLeft = document.createElement('div');
+        sectionLeft.classList.add('section');
+        sectionLeft.setAttribute('id', 'section-left');
+    const sectionCenter = document.createElement('div');
+        sectionCenter.classList.add('section');
+        sectionCenter.setAttribute('id', 'section-center');
+    const sectionRight = document.createElement('div');
+        sectionRight.classList.add('section');
+        sectionRight.setAttribute('id', 'section-right');
+    const footer = document.createElement('div');
+        footer.classList.add('footer');
+
+    container.appendChild(navbar);
+    container.appendChild(sectionLeft);
+    container.appendChild(sectionCenter);
+    container.appendChild(sectionRight);
+    container.appendChild(footer);
+}
